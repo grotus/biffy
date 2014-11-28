83a297f1b4a6e221ba8de16920a3c107381e93a0
Stefan Larsson @mac <slarsson.gm@gmail.com>
2014-11-28 21:44:39 +0100

    Attempt to disable minification of js on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,9 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier # UNCOMMENT FOR NORMAL MINIFICATION
+  config.assets.debug = true # COMMENT OUT FOR NORMAL MINIFICATION
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
