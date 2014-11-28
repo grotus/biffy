e94fc683e9f5bb3cf3f1cdfe9891c0e756b6c017
Stefan Larsson @mac <slarsson.gm@gmail.com>
2014-11-29 00:31:01 +0100

    Megarevert back to 7f31d1a

--- app/models/weight_reading.rb
+++
@@ -1,3 +0,0 @@
-class WeightReading < ActiveRecord::Base
-  belongs_to :user
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124224433) do
+ActiveRecord::Schema.define(version: 20141116184902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,15 +33,5 @@ ActiveRecord::Schema.define(version: 20141124224433) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "weight_readings", force: true do |t|
-    t.float    "weight"
-    t.datetime "entry_date"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "weight_readings", ["user_id"], name: "index_weight_readings_on_user_id", using: :btree
 
 end
