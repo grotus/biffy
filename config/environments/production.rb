484c8b504a0f97053a9cc5a36b7633a2bdb7adc8
Stefan Larsson @mac <slarsson.gm@gmail.com>
2014-11-28 21:57:20 +0100

    Attempt to disable minification, take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false # UNCOMMENT FOR NORMAL MINIFICATION
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier # UNCOMMENT FOR NORMAL MINIFICATION
@@ -29,10 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false # UNCOMMENT FOR NORMAL MINIFICATION
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true # UNCOMMENT FOR NORMAL MINIFICATION
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
